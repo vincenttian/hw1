6c4d67602dde93ea40deae74dfe5f90c9b2458d1
Howard Chen <howardxchen@gmail.com>
2014-09-27 15:34:30 -0700

    First commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'users#index'
+  get '/about', to: 'static_pages#about'
   get '/user/new', to: 'users#new', as: 'users'
   get '/user/:id', to: 'users#show', as: 'user'
   post '/user/new', to: 'users#create'
