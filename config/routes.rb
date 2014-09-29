d83496037b2faf9f4ebb68e308cb35953e9c40e2
Vincent Tian <vincenttian16@gmail.com>
2014-09-29 13:30:49 -0700

    hw1 except major column

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   #get '/about', to: 'CONTROLLER#FUNCTION'
   #get '/user/new', to: 'CONTROLLER#FUNCTION'
+  root 'users#index'
+  get '/user/new', to: 'users#new', as: 'users'
   get '/user/:id', to: 'users#show', as: 'user'
   post '/user/new', to: 'users#create'
+  get '/about', to: 'static_pages#about'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140929202755) do
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "age"
+  end
 
 end
